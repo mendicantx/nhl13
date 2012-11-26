32ccbf9d4b4fa3be911a3814f498fd392709cc05
mendicantx <mendicant@beigesunshine.com>
2012-11-26 11:25:09 -0800

    Create the rails app.

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Nhl13::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nhl13::Application.config.secret_token = '65e0f31077cec16b9ca151234bdf965430641e511706bc54290e7150cef819e030458a497f5253846692784c15c18ff25106a149859973e616467a3553063eb2'
